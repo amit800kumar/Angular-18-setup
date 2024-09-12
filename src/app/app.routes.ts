139b33a86a7c44a9364301ae4d94079d85d2a91a
Amit Kumar <eraks800@gmail.com>
2024-09-12 21:01:00 +0530

    routing fixes for child component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: '/feature1', pathMatch: 'full' },
     {
-        path: 'feature1', loadComponent: () => import('./features/feature1/feature1.component').then(m => m.Feature1Component), canActivate: [], children: [
+        path: 'feature1', children: [
+            { path: '', loadComponent: () => import('./features/feature1/feature1.component').then(m => m.Feature1Component) },
             { path: 'child', loadComponent: () => import('./features/feature1-child/feature1-child.component').then(m => m.Feature1ChildComponent) }
         ]
     },
