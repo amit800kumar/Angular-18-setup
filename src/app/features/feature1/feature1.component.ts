41484bc89aa420346494753204557ac3952b7582
Amit Kumar <eraks800@gmail.com>
2024-09-12 22:30:53 +0530

    routing fix

--- src/app/features/feature1/feature1.component.ts
+++ src/app/features/feature1/feature1.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-feature1',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './feature1.component.html',
   styleUrl: './feature1.component.css'
 })
